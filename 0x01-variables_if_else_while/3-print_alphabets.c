f85a421929c02c43f6dc42ddb61dbed96eddda12
Tomas Tecle <3thomas225@gmail.com>
2023-03-17 17:39:17 +0400

    small fixes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,23 +8,22 @@
  */
 int main(void)
 {
-        int i;
+	int i;
 	int x;
-	
+
 	i = 97;
 	x = 65;
 
-        while (i <= 122)
-        {
-                putchar(i);
-                i++;
-        }
-
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
 	while (x <= 90)
 	{
 		putchar(x);
 		x++;
 	}
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
