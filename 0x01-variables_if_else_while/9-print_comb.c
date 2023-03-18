42dfe0ac0259f53e27a327747eb8697d91e1da81
Tomas Tecle <3thomas225@gmail.com>
2023-03-18 05:11:57 +0400

    combinations of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 48;
+
+	while (x <= 57)
+	{
+		if (x == 57)
+		{
+			putchar(x);
+			x++;
+			continue;
+		} else
+		{
+			putchar(x);
+			putchar(',');
+		}
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
