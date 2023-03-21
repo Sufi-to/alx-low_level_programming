a3a92e2754473b200417bbfa70ac6b07de92f727
Tomas Tecle <3thomas225@gmail.com>
2023-03-22 02:23:12 +0400

    print the absolute value of an int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - Check Main
+ * @x: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+int abs(int x)
+{
+	if (x >= 0)
+	return (x);
+	else
+		return (x * -1);
+}
