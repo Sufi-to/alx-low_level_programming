501a0e9d263ce40a9a99977db3db93139a4f7532
Tomas Tecle <3thomas225@gmail.com>
2023-03-22 01:51:42 +0400

    prints 1 if letter is lower case and 0 if not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lower case character
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _islower(int c)
+{
+	return ( c >= 97 && c <= 122);
+}
