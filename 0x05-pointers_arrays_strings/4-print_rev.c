2644ca27ba7692dddd4e1789269c68711ca76f41
Tomas Tecle <3thomas225@gmail.com>
2023-03-27 17:51:49 +0400

    fixed task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,19 @@
 void print_rev(char *s)
 {
 	int i;
+	int len;
 
 	i = 0;
 	while (*(s + i) != '\0')
 	{
 		i++;
 	}
-	while (i >= 0)
+	len = i - 1;
+
+	while (len >= 0)
 	{
-		_putchar(*(s + i));
-		i--;
+		_putchar(*(s + len));
+		len--;
 	}
 	_putchar('\n');
 }
