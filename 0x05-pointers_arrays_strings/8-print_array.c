a88e251998d2cd7b23e71507eb3b0e58bf7ce914
Tomas Tecle <3thomas225@gmail.com>
2023-03-27 19:16:15 +0400

    task 8 and 9

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,5 +18,4 @@ void print_array(int *a, int n)
 			printf(", %d", a[i]);
 	}
 	printf("\n");
-	}
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -22,4 +22,6 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 	}
 	dest[len] = '\0';
+
+	return (dest);
 }
