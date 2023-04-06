cbdc3b00b109044b2f35418dcf3619636cd76684
Tomas Tecle <3thomas225@gmail.com>
2023-04-07 02:15:44 +0400

    task 6 and headerfil

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int fu_prime(int n, int i);
+/**
+ * is_prime_number - function to tell if a number is prime or not
+ * @n: input number to check
+ * Return: return 1 if the integer is prime and 0 if otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (fu_prime(n, n - 1));
+}
+
+/**
+ * fu_prime - findout if number is prime recursively
+ * @n: number to evaluate
+ * @i: loop iterator
+ * Return: 1 if number is prime, o if otherwise
+ */
+int fu_prime(int n, int i)
+{
+	if (n == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (fu_prime(n, i - 1));
+}
