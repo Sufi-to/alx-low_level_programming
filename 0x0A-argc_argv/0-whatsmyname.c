5a273e4b18f7046d12e1733bd3f34c15141994e8
Tomas Tecle <3thomas225@gmail.com>
2023-04-11 02:04:31 +0400

    argc and argv exercises with readme file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - displays program name
+ * @argc: number of arguments
+ * @argv: array of command line arguments
+ * Return: 0(Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the multiplication of 2 numbers'
+ * @argc: number of argumets
+ * @argv: array of arguments
+ * Return: if two arguments are not found print error and return 1
+ */
+int main(int argc, char **argv)
+{
+	int mul, x1, x2;
+
+	if (argc > 3 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]), atoi(argv[2]);
+	return (0);
+}
