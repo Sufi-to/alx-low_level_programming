c7c1abd9996659ac2889690a2da2d1e59b1a45c3
Tomas Tecle <3thomas225@gmail.com>
2023-04-12 00:21:50 +0400

    print a two dimentional array by allocating memory dynamically

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a two dimensional array
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer to the 2 dimensional array of int, NULL on failure or if
+ * height or width is zero
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i, j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(int *) * height);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		ptr [i] = malloc(sizeof(int) * width);
+		if (ptr[i] == NULL)
+		{
+			free(ptr);
+			for (j = 0; j <= 1; j++)
+				free(ptr[j]);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			ptr[i][j] = 0;
+	
+	return (ptr);
+}
