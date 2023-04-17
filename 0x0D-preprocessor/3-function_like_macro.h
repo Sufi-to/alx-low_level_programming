73a7af1535e01498594ff3f3e8ade36a1a74e0d8
Tomas Tecle <3thomas225@gmail.com>
2023-04-18 01:11:50 +0400

    absolute macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_
+#define FUNCTION_LIKE_MACRO_
 
-#define ABS(x) ((x) > (0) ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
