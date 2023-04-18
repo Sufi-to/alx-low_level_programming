471d103e2f7cb1173c7d310c7062fd24e2a686f1
Tomas Tecle <3thomas225@gmail.com>
2023-04-18 04:33:35 +0400

    tasks 2 and 3

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - pritns the struct dog
+ * @d: pointer to an instance of a struct dog
+ * Return: Nothing
+*/
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)\n");
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner != NULL)
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)\n");
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,13 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - new name(typedef) for struct dog
+*/
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
