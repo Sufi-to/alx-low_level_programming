b86a711918495ec07b8d1f877d3c53b82ed84a9c
Tomas Tecle <3thomas225@gmail.com>
2023-05-03 00:43:47 +0400

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,6 +4,7 @@
  * insert_nodeint_at_index - inserts a new node at a given link
  * @head: double pointer to the head of the linked list
  * @idx: index where the node is inserted
+ * @n: data to be put in the new node
  * Return: address of new node or NULL if it failed
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
