6820263ec3d40f28a69597ab03fac7d3ac1ff8ee
Sufi-to <3thomas225@gmail.com>
2023-11-27 19:38:39 +0400

    create file and write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * create_file - function that creates a file
- * @filename: pointer to filename
+ * @filename: pointer to filename to be created
  * @text_content: pointer to string that will be written on file
  * Return: -1 for failure, 1 for success or Null if filename
  * is null
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 		}
 	}
-	open_int = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	open_int = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_bts = write(open_int, text_content, len);
 	if (open_int < 0 || write_bts < 0)
 		return (-1);
