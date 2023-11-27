235b07ab5c3c21fabf56747f05ec68aa150286d3
Sufi-to <3thomas225@gmail.com>
2023-11-27 19:13:18 +0400

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -24,7 +24,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		close(open_f);
 		return (1);
 	}
-	write_f = write(open_f, text_content, sizeof(text_content) - 1);
+	write_f = write(open_f, text_content, (sizeof(text_content) - 1));
 	if (write_f < 0)
 	{
 		close(open_f);
