168a58b3e0e65f821eb50149a9937afa008aff75
Sufi-to <3thomas225@gmail.com>
2023-11-27 18:16:14 +0400

    create and write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * create_file - function that creates a file
  * @filename: pointer to filename
@@ -12,21 +11,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
-	if (filename && text_content==NULL)
+	if (filename && text_content == NULL)
 	{
-		open_int = open(filename, O_CREAT| O_TRUNC);
+		open_int = open(filename, O_CREAT | O_TRUNC);
 		if (open_int > 0)
 		{
 			close(open_int);
 			return (1);
 		}
 		else
-		{
 			return (-1);
-		}
 	}
-
 	if (filename && text_content != NULL)
 	{
 		open_int = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -45,12 +40,11 @@ int create_file(const char *filename, char *text_content)
 			}
 			else
 			{
-				close (open_int);
+				close(open_int);
 				return (1);
 			}
 		}
 	}
-
 	return (0);
 }
 
