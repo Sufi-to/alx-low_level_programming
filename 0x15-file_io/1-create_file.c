31b20e61915dc43145758cacbf37c2f9f49463f6
Sufi-to <3thomas225@gmail.com>
2023-11-27 19:44:45 +0400

    create file and write

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int open_int = 0, write_bts = 0, len;
+	int open_int = 0, write_bts = 0, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-		{
+		while (text_content[len])
 			len++;
-		}
 	}
 	open_int = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	write_bts = write(open_int, text_content, len);
 	if (open_int < 0 || write_bts < 0)
+	{
+		close(open_int);
 		return (-1);
+	}
 
 	close(open_int);
 
