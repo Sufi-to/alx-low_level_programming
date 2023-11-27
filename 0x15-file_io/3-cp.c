85122ffe6f52fe3101fa12e5e0a566ef2e3f41ae
Sufi-to <3thomas225@gmail.com>
2023-11-28 00:01:05 +0400

    copy from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,10 +32,9 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	open_from = open(argv[1], O_RDONLY);
 	if (open_from < 0)
 	{
